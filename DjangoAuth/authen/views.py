63eea5dc89fe467049caa666931e3eb77eb1b744
Abhishek <abhiccc5@gmail.com>
2020-08-06 14:25:08 +0530

    fetched all users

--- DjangoAuth/authen/views.py
+++ DjangoAuth/authen/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
  
 # Create your views here.
 def signup_view(request):
@@ -9,7 +11,8 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return render(request, 'authen/dashboard.html', {'form': form})
+            # return render(request, 'authen/dashboard.html',{})
+            return get_users_view(request)
     else:
         form = UserCreationForm()
     return render(request, 'authen/signup.html', {'form': form})
@@ -23,7 +26,8 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return render(request, 'authen/dashboard.html')
+            # return render(request, 'authen/dashboard.html',{})
+            return get_users_view(request)
     else:
         form = AuthenticationForm()
     return render(request, 'authen/login.html', {'form': form})
@@ -32,3 +36,9 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('/landing')
+
+
+def get_users_view(request):
+    all_users= get_user_model().objects.all()
+    context= {'all_users': all_users}
+    return render(request, 'authen/dashboard.html', context)
